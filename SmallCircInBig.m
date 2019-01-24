10b737253460f536d58cf30f7170957a408a1c08
KittyL <YLi@gmail.com>
2019-01-24 05:17:39 -0500

    Added the featuer of choosing any other tracing point in the small circle.

--- SmallCircInBig.m
+++ SmallCircInBig.m
@@ -1,4 +1,4 @@
-function SmallCircInBig(R,r,rot)
+function SmallCircInBig(R,r,pos,rot)
 % Draw a big circle centered at the origin and a smaller inner circle 
 % touching it at (R,0). The smaller circle rolls clockwise along the big
 % circle's circumference. The program traces the movement of the point Ps
@@ -6,11 +6,16 @@ function SmallCircInBig(R,r,rot)
 % Input parameter:
 % R: radius of the big circle;
 % r: radius of the small circle;
+% pos: the point to trace; indicated by the distance from the center of the 
+% small circle; 0<pos<r;
 % rot: number of rotations the small circle rolls around;
 
 
 % figure('visible','on'); This command is needed if the commands are
 % implemented in live script; 
+if pos > r
+    error('The trace point must be inside the small circle! (pos <= r)');
+end
 axis([-R R -R R]);
 % plot the big circle;
 fimplicit(@(x,y) x.^2 + y.^2 - R^2);
@@ -21,7 +26,7 @@ s = 0:0.1:2*pi;
 xsmall = R-r+r*cos(s);
 ysmall = R-r+r*sin(s);
 hsmall = plot(xsmall,ysmall,'r');
-p = plot(xsmall(1),ysmall(1),'.','MarkerFaceColor','red');
+p = plot(R-r+pos,0,'.','MarkerFaceColor','red');
 % t is the parameter angle formed by the radius of the big circle through 
 % the center of the smaller circle and the radius of the smaller circle 
 % through the point Ps;
@@ -35,8 +40,8 @@ for ii =1:length(t)
     ysmallmov = (R-r)*sin(alpha(ii))+r*sin(s);
     hsmall.XData = xsmallmov;
     hsmall.YData = ysmallmov;
-    p.XData = (R-r)*cos(alpha(1:ii))+r*cos(alpha(1:ii)-t(1:ii));
-    p.YData = (R-r)*sin(alpha(1:ii))+r*sin(alpha(1:ii)-t(1:ii));
+    p.XData = (R-r)*cos(alpha(1:ii))+pos*cos(alpha(1:ii)-t(1:ii));
+    p.YData = (R-r)*sin(alpha(1:ii))+pos*sin(alpha(1:ii)-t(1:ii));
     drawnow;
 end
 axis equal;
