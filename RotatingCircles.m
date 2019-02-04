5f537acb2d1c12c490e3351f1b6e713bba1d2914
KittyL <YLi@gmail.com>
2019-02-04 14:18:54 -0500

    fix issue #404230804. Change the boundaries to accommodate the circles.

--- RotatingCircles.m
+++ RotatingCircles.m
@@ -1,4 +1,4 @@
-function RotatingCircles(r,InOutInd,pos,rot,spd)
+function RotatingCircles(r,InOutInd,pos,rot,spdfactor,spd)
 % Draw the unit circle C0 centered at the origin, followed by a sequence of 
 % circles Ci, all touching the previous one, either internally, or 
 % externally on the right. If a circle touches the previous one internally,
@@ -19,10 +19,12 @@ function RotatingCircles(r,InOutInd,pos,rot,spd)
 % pos: the point to trace; indicated by the distance from the center of Cn; 
 % 0<pos<r(n);
 % rot: number of rotations C1 rolls around;
-% speed: a vector of n-1 components, each of the speed factor  
+% spdfactor: a vector of n-1 components, each of the speed factor  
 % describes how faster C_i+1 rolls than Ci; 
-% if speed = 1, they rolls the same number of rotations during the same 
+% if spdfactor = 1, they rolls the same number of rotations during the same 
 % time period;
+% spd: control the speed of the whole rotation; it is the pause between
+% each move, spd>=0.
 
 
 % figure('visible','on'); This command is needed if the commands are
@@ -33,7 +35,17 @@ if pos > r(numCirc)
     error('The trace point must be inside the last circle! (pos <= r(n))');
 end
 fimplicit(@(x,y) x.^2 + y.^2 - 1);
-Rlim = 1+2*sum(r(InOutInd<0));
+% compute the region that accommodate all circles; 
+Llim = 1;
+Rlim = 1;
+for ii = 1:numCirc
+    if InOutInd < 0
+        Rlim = Rlim+2*r(ii);
+    else
+        Llim = max(Llim,abs(Rlim-2*r(ii)));
+    end
+end
+Rlim = max(Llim,Rlim);
 axis equal;
 axis([-Rlim Rlim -Rlim Rlim]);
 hold on;
@@ -41,27 +53,20 @@ axis manual;
 % plot the initial sequence of circles;
 s = 0:2*pi/50:2*pi;
 h = gobjects(1,numCirc);
-xcenterout = 1;
-xoutpre = 0;
-for ii = 1:numCirc
-    if InOutInd(ii) == 1
-        xcenter = 1-r(ii);
-        x = xcenter+r(ii)*cos(s);
-    else 
-        xcenterout = xcenterout+xoutpre+r(ii);
-        xoutpre = r(ii);
-        x = xcenterout+r(ii)*cos(s);
-    end
+xcenter = 1-InOutInd(1)*r(1);
+x = xcenter+r(1)*cos(s);
+y = r(1)*sin(s);
+h(1) = plot(x,y,'r');
+for ii = 2:numCirc
+    xcenter = xcenter+r(ii-1)-InOutInd(ii)*r(ii);
+    x = xcenter+r(ii)*cos(s);
     y = r(ii)*sin(s);
     h(ii) = plot(x,y,'r');
 end
 % plot the point to trace in the last circle;
-if InOutInd(numCirc) == 1
-    p = plot(1-r(numCirc)+pos,0,'.','MarkerFaceColor','red');
-else
-    p = plot(xoutpre-pos,0,'.','MarkerFaceColor','red');
-end
-%
+p = plot(xcenter+InOutInd(numCirc)*pos,0,'o','MarkerFaceColor','red');
+
+
 % t(i) is the parameter angle formed by the radius of C(i-1) through the
 % center of Ci and the radius of Ci through the point Pi;
 % alpha(i) is the angle formed by the radius of C(i-1) through the center 
@@ -70,7 +75,7 @@ numSampInt = 200;
 t = 0:2*pi/numSampInt:rot*2*pi;
 numSampt = length(t);
 t = repmat(t,numCirc,1);
-t = diag([1,spd])*t;
+t = diag([1,spdfactor])*t;
 alpha = zeros(numCirc,numSampt);
 alpha(1,:)=r(1)*t(1,:)/1;
 for ii = 2:numCirc
@@ -81,7 +86,7 @@ lastxcenter = zeros(1,numSampt);
 lastycenter = zeros(1,numSampt);
 anglecum = zeros(1,numSampt);
 for ii = 1:numSampt
-    % pause(0.01);
+    pause(spd);
     % draw C1 after rotation;
     xcenter = (1-InOutInd(1)*r(1))*cos(alpha(1,ii));
     ycenter = (1-InOutInd(1)*r(1))*sin(alpha(1,ii));
@@ -99,8 +104,8 @@ for ii = 1:numSampt
     lastxcenter(ii) = xcenter;
     lastycenter(ii) = ycenter;
     % trace the point pos;
-    p.XData = lastxcenter(1:ii)+pos*cos(InOutInd(numCirc)*(anglecum(1:ii)-InOutInd(numCirc)*t(numCirc,1:ii)));
-    p.YData = lastycenter(1:ii)+pos*sin(InOutInd(numCirc)*(anglecum(1:ii)-InOutInd(numCirc)*t(numCirc,1:ii)));
+    p.XData = lastxcenter(1:ii)+InOutInd(numCirc)*pos*cos(anglecum(1:ii)-InOutInd(numCirc)*t(numCirc,1:ii));
+    p.YData = lastycenter(1:ii)+InOutInd(numCirc)*pos*sin(anglecum(1:ii)-InOutInd(numCirc)*t(numCirc,1:ii));
     drawnow;
 end
 end
